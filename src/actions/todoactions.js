06ba3155f15e620b54c1350d079c19353841ba23
ankit mishra <mishraankit987@gmail.com>
2023-11-30 17:24:46 +0530

    todoList reduxStore devtools

--- src/actions/todoactions.js
+++
@@ -1,5 +0,0 @@
-import { addTodo,editTodo,finishTodo,delTodo } from "../constants/constants"
-export const ADD_TODO=(editText)=>({type:addTodo,payload:{todoText:editText}})
-export const DEL_TODO=(todoId)=>({type:delTodo,payload:{todoId}})
-export const FINISH_TODO=(todo,isFinished)=>({type:finishTodo,payload:{todo,isFinished}})
-export const EDIT_TODO=(todo,todoText)=>({type:editTodo,payload:{todo,todoText}})
--- src/constants/constants.js
+++
@@ -1,4 +0,0 @@
-export const addTodo='add_todo';
-export const delTodo='delete_todo';
-export const finishTodo='change_finish'
-export const editTodo='edit_todo'
